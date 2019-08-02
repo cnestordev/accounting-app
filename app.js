06cbc3dcbdda4632e8ea0c08688fad59c4812626
Nestor Cervantes <nestor@Nestors-MacBook-Air.local>
2019-08-02 00:52:21 -0700

    work on dark mode

--- app.js
+++ app.js
@@ -442,6 +442,6 @@ app.get("*", function(req, res){
 
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 2000, function() {
   console.log("Server started on port 3000");
 });
